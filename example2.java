7cbba89dbe4e4eac5befea6627f867bc270b1480
SaiRishitha29 <sairishithababburi2685@gmail.com>
2025-03-02 23:36:53 +0530

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,9 @@
+class example2{
+
+	public static void main(String[] args){
+		System.out.println("Name: Sai RISHITHA");
+		System.out.println("Roll.no: 24016");
+		System.out.println("Branch:CSE");
+		System.out.println("Section:A");
+	}
+}
